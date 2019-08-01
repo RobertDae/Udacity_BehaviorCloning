2f07977f5f6fcfe9a90ab7bf6ee91589e27ad10e
Robert D <robertrd@gmx.de>
2019-08-01 22:26:21 +0200

    Update

--- model_cnn.py
+++ model_cnn.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 
 
 class Pipeline:
-    def __init__(self, model=model, base_path='', epochs=2):   #model=None replaced with model =model()
+    def __init__(self, model=None, base_path='', epochs=2):   #model=None replaced with model =model()
         self.data = []
         self.model = model
         self.epochs = epochs
